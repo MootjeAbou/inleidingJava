7c8f90bcacf75ffea941ae4a3afa4681ebcb4c7a
Mohammed <melmouhidi@roc-dev.nl>
2019-09-26 09:57:45 +0200

    22/9

--- src/H6/h6_1.java
+++ src/H6/h6_1.java
@@ -1,4 +1,19 @@
 package H6;
 
-public class h6_1 {
+import java.awt.*;
+import java.applet.*;
+
+
+public class h6_1 extends Applet {
+    int teller;
+
+    public void init() {
+        teller = 0;
+    }
+
+    public void paint(Graphics g) {
+        teller++;
+        g.drawString("Scherm is " + teller +
+                " keer ververst.", 50, 60);
+    }
 }
